8c3ccbc52935359e1de9c29fb95bf8ea95dcf7d5
Joshua Baxter <baxter.josh@gmail.com>
2017-04-19 22:09:58 -0700

    early experiments; single input, single output

--- win2d_speech_recognition/MainPage.xaml.cs
+++ win2d_speech_recognition/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.SpeechRecognition;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -15,16 +18,56 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace win2d_speech_recognition
-{
+namespace win2d_speech_recognition {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+    public sealed partial class MainPage : Page {
+        public MainPage() {
             this.InitializeComponent();
+        }
+
+        string str = "nada";
+        private async Task RecordSpeechFromMicrophoneAsync() {
+            var speechRecognizer = new SpeechRecognizer();
+            speechRecognizer.Timeouts.InitialSilenceTimeout = new TimeSpan(0, 1, 0);
+                //UIOptions.IsReadBackEnabled = false;
+
+            await speechRecognizer.CompileConstraintsAsync();
+            SpeechRecognitionResult speechRecognitionResult = await speechRecognizer.RecognizeAsync();//RecognizeWithUIAsync();
+            
+            str = speechRecognitionResult.Text;
+            //var messageDialog = new Windows.UI.Popups.MessageDialog(speechRecognitionResult.Text, "Text spoken");
+            //await messageDialog.ShowAsync();
+        }
+
+        private void canvasMain_Draw(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedDrawEventArgs args) {
+            args.DrawingSession.DrawText(str, new System.Numerics.Vector2(10, 10), Colors.White);
+        }
+
+        private void canvasMain_Update(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedUpdateEventArgs args) {
+
+        }
+
+        private void canvasMain_CreateResources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args) {
+
+        }
+
+        private void canvasMain_PointerMoved(object sender, PointerRoutedEventArgs e) {
+
+        }
+
+        private void canvasMain_PointerPressed(object sender, PointerRoutedEventArgs e) {
+
+        }
+
+        private void canvasMain_PointerReleased(object sender, PointerRoutedEventArgs e) {
+
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e) {
+            await RecordSpeechFromMicrophoneAsync();
+            int i = 0;
         }
     }
 }
