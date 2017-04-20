3acd9ddfb412d6c2d1c588dff90dd1a0a658ba5a
Joshua Baxter <baxter.josh@gmail.com>
2017-04-19 23:49:36 -0700

    minor y-axis randomness adjustments for individual characters

--- win2d_speech_recognition/AnimatedString.cs
+++ win2d_speech_recognition/AnimatedString.cs
@@ -11,10 +11,12 @@ namespace win2d_speech_recognition {
     class AnimatedString {
         private List<AnimatedCharacter> _characters = new List<AnimatedCharacter>();
         public AnimatedString(CanvasDevice device, string str, Vector2 position) {
+            Random r = new Random(DateTime.Now.Millisecond);
             float x = position.X;
             foreach (char c in str) {
+                float y = position.Y + 10 - r.Next(20);
                 if (c != ' ') {
-                    _characters.Add(new AnimatedCharacter(c, new Vector2(x, position.Y)));
+                    _characters.Add(new AnimatedCharacter(c, new Vector2(x, y)));
                 }
                 x += 90;
             }
